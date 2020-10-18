5f7bf79a088f68e952c9f4a4d770d691e1edaf88
jamie16oreilly@gmail.com <jamie16oreilly@gmail.com>
2020-10-18 17:56:32 +0100

    Modularised code and joined results from scraping clerk website/pdfs

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # financial-disclosure-scraper
 Scrapes US House of Clerks financial disclosure page (as well as pdfs).
 
-![View of investment page (Pelosi) ](https://i.paste.pics/70e35d57c2c2affa414e51b97f8447d4.png)
+![View of investment page (Pelosi) ](https://snipboard.io/uUheZP.jpg)
 
-![View of specific filing (Pelosi)](https://i.paste.pics/dc7afeac599182dd8f265b822d20fed6.png)
--- src/main.py
+++ src/main.py
@@ -1,57 +1,8 @@
-import requests
+import scraper.helpers as sch
 import pandas as pd
-import lxml
-from bs4 import BeautifulSoup
-from tika import parser
-import io
-import re
 
-data = requests \
-    .post("https://disclosures-clerk.house.gov/PublicDisclosure/FinancialDisclosure/ViewMemberSearchResult", data ={"LastName": "Pelosi"}).text
+# Generates image from readme
+# There is a slice on the collect_filings function - remove it if 
+# you want more rows
 
-#%%
-
-soup = BeautifulSoup(data, 'lxml')
-
-lista = []
-
-for link in soup.find_all('tr'):
-    row = {}
-    for foo in link.find_all('td'):
-        if foo.find('a') is not None:
-            row.update({'href': foo.find('a').get('href'), foo.get('data-label'): foo.text})
-        else:
-            row.update({foo.get('data-label'): foo.text})
-    print(row, '\n')
-    lista.append(row)
-    
-df = pd.DataFrame(lista[1:])   
-
-#%%
-
-rel_pdf = requests.get('https://disclosures-clerk.house.gov/public_disc/financial-pdfs/2016/10015814.pdf')
-
-#%%
-
-with io.BytesIO(rel_pdf.content) as open_file:
-    raw = parser.from_buffer(open_file)
-
-#%%
-
-
-vx = re.findall("\\n(((?!\\n).)*)\s\w{2}\s(\$[0-9\,]+.*?\$[0-9\,]+)", raw["content"], flags=re.I | re.S | re.M)
-
-df2 = pd.DataFrame(vx, columns=["Name", "Trash", "Investment"]).drop("Trash", axis=1)
-
-df2 = df2[df2["Name"] != ""]
-
-df2 = df2[df2["Name"].str.lower().str.find('description') == -1]
-
-
-
-
-
-
-
-
-
+data = sch.collect_filings('Pelosi')
